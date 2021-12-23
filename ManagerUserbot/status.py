50a966c6f2ee464c6c511ab49b656e906a541f43
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-12-23 09:56:20 +0700

    Update status.py

--- ManagerUserbot/status.py
+++ ManagerUserbot/status.py
@@ -14,5 +14,5 @@ def is_admin(func):
         if is_admin:
             await func(event, _s)
         else:
-            await event.reply("Only Admins can execute this command!")
+            await event.reply("Hanya Admin Yang bisa Mengatur")
     return a_c
