8177345a29d7402b7b85f1ef5befa3c9259b1c6c
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2021-12-23 09:54:58 +0700

    Update and rename Configs.py to Config.py

--- ManagerUserbot/Config.py
+++ ManagerUserbot/Config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     APP_ID = int(os.environ.get("APP_ID", 6))
     API_HASH = os.environ.get("API_HASH", None)
-    TOKEN = os.environ.get("TOKEN", None)
+    STRING_SESSION = os.environ.get("STRING_SESSION", None)
     BOT_US = os.environ.get("BOT_US", None)
     WELCOME_TEXT = os.environ.get("WELCOME_TEXT", None)
     RULES = os.environ.get("RULES", None)
